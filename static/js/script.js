291b85ff06979452f2eaadd7efca5beacfe7279b
Tina Huynh <tinamchuynh@gmail.com>
2021-12-14 12:10:00 -0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-    $('ul#filter button').click(function() {
-        $(this).css('outline', 'none');
-        $('ul#filter .current').removeClass('current');
-        $(this).parent().addClass('current');
+    $("ul#filter button").click(function() {
 
-        var filterVal = $(this).text().toLowerCase().replace(' ', '-');
 
-        if (filterVal == 'all') {
-            $('ul#portfolio li.hidden').fadeIn('slow').removeClass('hidden');
+
+        $("ul#filter .current").removeClass("current");
+        $(this).parent().addClass("current");
+
+        var filterVal = $(this).text().toLowerCase().replace(" ", "-");
+
+        if (filterVal == "all") {
+            $("ul#portfolio li.hidden").fadeIn("slow").removeClass("hidden");
         } else {
-            $('ul#portfolio li').each(function() {
+            $("ul#portfolio li").each(function() {
                 if (!$(this).hasClass(filterVal)) {
-                    $(this).fadeOut('normal').addClass('hidden');
+                    $(this).fadeOut("normal").addClass("hidden");
                 } else {
-                    $(this).fadeIn('slow').removeClass('hidden');
+                    $(this).fadeIn("slow").removeClass("hidden");
                 }
             });
         }
