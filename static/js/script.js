f753b15af9461a20aeba62185affcf1fcbdf5fd5
Tina Huynh <tinamchuynh@gmail.com>
2021-12-13 22:15:32 -0800

    Attempt another method

--- static/js/script.js
+++ static/js/script.js
@@ -1,79 +1,23 @@
 $(document).ready(function() {
+    $('ul#filter a').click(function() {
+        $(this).css('outline', 'none');
+        $('ul#filter .current').removeClass('current');
+        $(this).parent().addClass('current');
 
-    var items = $('#stage li'),
-        itemsByTags = {};
+        var filterVal = $(this).text().toLowerCase().replace(' ', '-');
 
-    // Looping though all the li items:
+        if (filterVal == 'all') {
+            $('ul#portfolio li.hidden').fadeIn('slow').removeClass('hidden');
+        } else {
+            $('ul#portfolio li').each(function() {
+                if (!$(this).hasClass(filterVal)) {
+                    $(this).fadeOut('normal').addClass('hidden');
+                } else {
+                    $(this).fadeIn('slow').removeClass('hidden');
+                }
+            });
+        }
 
-    items.each(function(i) {
-        var elem = $(this),
-            tags = elem.data('tags').split(',');
-
-        // Adding a data-id attribute. Required by the Quicksand plugin:
-        elem.attr('data-id', i);
-
-        $.each(tags, function(key, value) {
-
-            // Removing extra whitespace:
-            value = $.trim(value);
-
-            if (!(value in itemsByTags)) {
-                // Create an empty array to hold this item:
-                itemsByTags[value] = [];
-            }
-
-            // Each item is added to one array per tag:
-            itemsByTags[value].push(elem);
-        });
-
+        return false;
     });
-
-    function createList(text, items) {
-
-        // This is a helper function that takes the
-        // text of a menu button and array of li items
-
-        // Creating an empty unordered list:
-        var ul = $('<ul>', { 'class': 'hidden' });
-
-        $.each(items, function() {
-            // Creating a copy of each li item
-            // and adding it to the list:
-
-            $(this).clone().appendTo(ul);
-        });
-
-        ul.appendTo('#container');
-
-        // Creating a menu item. The unordered list is added
-        // as a data parameter (available via .data('list')):
-
-        var a = $('<a>', {
-            html: text,
-            href: '#',
-            data: { list: ul }
-        }).appendTo('#filter');
-    }
-    // Creating the "Everything" option in the menu:
-    createList('Everything', items);
-
-    // Looping though the arrays in itemsByTags:
-    $.each(itemsByTags, function(k, v) {
-        createList(k, v);
-    });
-
-    $('#filter a').live('click', function(e) {
-        var link = $(this);
-
-        link.addClass('active').siblings().removeClass('active');
-
-        // Using the Quicksand plugin to animate the li items.
-        // It uses data('list') defined by our createList function:
-
-        $('#stage').quicksand(link.data('list').find('li'));
-        e.preventDefault();
-    });
-
-    // Selecting the first menu item by default:
-    $('#filter a:first').click();
-})
+});
