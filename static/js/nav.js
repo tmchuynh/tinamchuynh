7729194cbb2187e21dad0204400044a799baf099
tmchuynh <tinamchuynh@gmail.com>
2022-04-12 06:58:50 -0700

    backtotop functionality

---
+++ static/js/nav.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Start of Back to Top functions
+var mybutton = document.getElementById("backToTop");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    // Back to Top button onclick function
+    document.documentElement.style.scrollBehavior = "smooth";
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    /////// Prevent closing from click inside dropdown
+    document.querySelectorAll('.dropdown-menu').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.stopPropagation();
+        });
+    })
+});
+
+var toggler = document.getElementsByClassName("navbar-toggler");
+console.log(toggler);
+toggler.onclick = function() {
+    let mobile = document.getElementsByClassName("navbar-collapse collapse");
+    console.log(mobile);
+}
+// End
