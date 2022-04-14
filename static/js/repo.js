7618801b356652f296ee0ad097fa7c93d0bf8947
tmchuynh <tinamchuynh@gmail.com>
2022-04-14 09:27:48 -0700

    Finish importing projects from github

--- static/js/repo.js
+++ static/js/repo.js
@@ -4,66 +4,47 @@ var obj = new Array();
 $.getJSON('https://api.github.com/users/tmchuynh/repos', (data) => {
     console.log(data);
 
-    
-    for (var obj = 0; obj < data.length; obj++) {
-        if (data[obj].visibility === "public") {
-            
-            // console.log(data[i].html_url); // urls of repos
-            var url = data[obj].html_url;
-
-            // console.log(data[i].name); // name of project
-            var proj_name = data[obj].full_name;
-
-            // console.log(data[i].pushed_at); // last updated
-            var updated = data[obj].pushed_at;
-
-            var language;
-
-            $.getJSON(data[obj].languages_url, (lang) => {
-                // console.log(lang); // array of languages used
-                language = lang;
-                obj = [proj_name, language, url, updated];
-
-
-                var card = document.createElement("div");
-
-                // card.classList.add("d-flex");    
-                card.classList.add("cards");
-                
-                var title = document.createElement("a");
-                title.innerHTML = proj_name;
-                console.log(title);
-
-                var hyperlink = document.createAttribute("href");
-                hyperlink.value = url;
-                title.setAttributeNode(hyperlink);
-
-                card.appendChild(title);
-
-                for (let i = 0; i < language[1]; i++) {
-                    var langs = document.createElement("h4");
-                    langs.innerHTML = project[1][0][i];
-
-                    card.appendChild(langs);
-                }
-
-                var last_updated = document.createElement("code");
-                last_updated.innerHTML = updated;
-
-                card.appendChild(last_updated);
-                project_container.appendChild(card);
-            })
-
-        }
-        
-    }
-    
+    data.forEach((element) => {
+        populate(element.name, element.html_url, element.pushed_at);
+    })
 })
 
 
+function populate(name, url, updated) {
 
-function populate(project) {
-    console.log(project);
+    var card = document.createElement("div");
+    card.classList.add("cards");
 
+    var title = document.createElement("a");
+    title.classList.add("title");
+    title.innerHTML = name;
+    var hyperlink = document.createAttribute("href");
+    hyperlink.value = url;
+    title.setAttributeNode(hyperlink);
+    card.appendChild(title);
+
+    console.log(updated);
+    date = updated.split("T")[0]
+    console.log(date);
+    date_0 = date.split("-");
+
+    year = date_0[0];
+    month = date_0[1];
+    day = date_0[2];
+
+    console.log(month, " ", day, " ", year)
+
+    var subtitle = document.createElement("code");
+    subtitle.classList.add("last-updated");
+    subtitle.classList.add("mb-0");
+    subtitle.innerHTML = "Last updated on:"
+    card.appendChild(subtitle);
+
+    var last_updated = document.createElement("code");
+    last_updated.innerHTML = month + "/" + day + "/" + year;
+    last_updated.classList.add("last-updated");
+    last_updated.classList.add("mt-0");
+    card.appendChild(last_updated);
     
+    project_container.appendChild(card);
 }
