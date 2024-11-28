f5d45621f6259faced4609de870598aca7749872
Tina Huynh <tinamchuynh@gmail.com>
2024-11-27 17:40:51 -0800

    change data

--- src/data/types.tsx
+++ src/data/types.tsx
@@ -1,6 +1,7 @@
 export interface ImageProps {
   src: string;
   alt: string;
+  description: string;
 }
 
 export interface PortfolioProject {
@@ -9,10 +10,10 @@ export interface PortfolioProject {
   description: string;
   features?: string[];
   focuses?: string[];
-  technologies: string[];
+  technologies?: string[];
   images: ImageProps[];
   timeline?: string[];
   inspiration?: string[];
-  githubLink: string;
-  liveLink: string;
+  githubLink?: string;
+  liveLink?: string;
 }
