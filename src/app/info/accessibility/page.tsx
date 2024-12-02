cac70cb3bfe405f469f0df18ffce3fe54a971aea
Tina Huynh <tinamchuynh@gmail.com>
2024-12-01 18:30:55 -0800

    move page

--- src/app/info/accessibility/page.tsx
+++ src/app/info/accessibility/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import BlurFade from "@/components/ui/blur-fade";
+import Breadcrumb from "@/components/ui/breadcrumb";
 import React from "react";
+
+const breadcrumbItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About Me",
+    dropdownItems: [
+      { label: "Who I Am", href: "/about" },
+      { label: "Accessibility Declaration", href: "/info/accessibility" },
+      { label: "Resume", href: "/info/resume" },
+      { label: "Tech Stack", href: "/info/techStack" },
+      { label: "Achievements", href: "/info/experiences&achievements" },
+    ],
+  },
+  {
+    label: "Details",
+  },
+];
 
 const AccessibilityPage = () => {
   return (
     <div className="max-w-7xl mx-auto p-6">
+      <Breadcrumb items={breadcrumbItems} />
+
       <BlurFade delay={0.25}>
         <h1 className="text-4xl font-bold text-center mb-6">Accessibility Declaration</h1>
       </BlurFade>
