a2bbfab763381aab2aa2d7fdf1d12438130ec37f
Tina Huynh <tinamchuynh@gmail.com>
2024-12-01 18:31:10 -0800

    add breadcrumbs

--- src/app/info/experiences&achievements/page.tsx
+++ src/app/info/experiences&achievements/page.tsx
@@ -1,11 +1,33 @@
 import AchievementsPage from '@/components/AchievementsPage';
+import Breadcrumb from '@/components/ui/breadcrumb';
 import { awards, certifications, milestones, skillBadges } from '@/data/data';
 import React from 'react';
+
+const breadcrumbItems = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About Me",
+    dropdownItems: [
+      { label: "Who I Am", href: "/about" },
+      { label: "Accessibility Declaration", href: "/info/accessibility" },
+      { label: "Resume", href: "/info/resume" },
+      { label: "Tech Stack", href: "/info/techStack" },
+      { label: "Achievements", href: "/info/experiences&achievements" },
+    ],
+  },
+  {
+    label: "Details",
+  },
+];
 
 const Page = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-center mb-8">My Relevant Experiences</h1>
+      <Breadcrumb items={breadcrumbItems} />
+
       <AchievementsPage awards={awards} skillBadges={skillBadges} milestones={milestones} certifications={certifications} />
     </div >
   );
