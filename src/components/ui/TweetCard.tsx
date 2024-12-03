862beab48e305bd685394ee26a0ccad5989c7e4b
Tina Huynh <tinamchuynh@gmail.com>
2024-12-03 00:05:36 -0800

    add alt

--- src/components/ui/TweetCard.tsx
+++ src/components/ui/TweetCard.tsx
@@ -215,6 +215,7 @@ export const TweetMedia = ( { tweet }: { tweet: EnrichedTweet; } ) => (
           // @ts-expect-error
           src={tweet.card.binding_values.thumbnail_image_large.image_value.url}
           className="h-64 rounded-xl border object-cover shadow-sm"
+          alt=""
         />
       )}
   </div>
